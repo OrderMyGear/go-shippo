10bac868ceb26b3f5458a8de1012b8daa2fc2b10
Phillip Burch <phillip.burch@live.com>
2018-06-25 15:46:54 -0500

    Add new api version

--- shippo.go
+++ shippo.go
@@ -6,6 +6,7 @@ const (
 	APIVersion20140211 = "2014-02-11"
 	APIVesrion20161025 = "2016-10-25"
 	APIVersion20170329 = "2017-03-29"
+	APIVersion20180208 = "2018-02-08"
 )
 
 // NewClient creates a new Shippo client.
