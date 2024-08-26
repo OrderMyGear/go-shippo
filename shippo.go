1281c3dc77fdc4a31d3e525cdb61018a728deb41
slavick <slavick@ordermygear.com>
2024-08-26 16:56:22 -0500

    Fix typos

--- shippo.go
+++ shippo.go
@@ -4,7 +4,7 @@ import "github.com/OrderMyGear/go-shippo/client"
 
 const (
 	APIVersion20140211 = "2014-02-11"
-	APIVesrion20161025 = "2016-10-25"
+	APIVersion20161025 = "2016-10-25"
 	APIVersion20170329 = "2017-03-29"
 	APIVersion20180208 = "2018-02-08"
 )
