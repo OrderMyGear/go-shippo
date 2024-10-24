1dffbae16a812f01ad02b2a12374c58ecb73a364
slavick <slavick@ordermygear.com>
2024-10-24 11:06:24 -0500

    Truncate first and last name fields when creating new shippo sub accounts

--- client/sub_account.go
+++ client/sub_account.go
@@ -11,6 +11,14 @@ func (c *Client) CreateSubAccount(input *models.SubAccountInput) (*models.SubAcc
 		return nil, errors.New("nil input")
 	}
 
+	// Truncate first and last name to 30 characters to avoid Stripe API errors
+	if len(input.FirstName) > 30 {
+		input.FirstName = input.FirstName[:30]
+	}
+	if len(input.LastName) > 30 {
+		input.LastName = input.LastName[:30]
+	}
+
 	output := &models.SubAccount{}
 	err := c.do(http.MethodPost, "/shippo-accounts", input, output, nil)
 	return output, err
